6035f30c9607fd7168cc1fc3dee70b6086e3f9c4
Lucas Moidel <lucas122133@gmail.com>
2026-02-11 13:40:47 -0800

    Update transmit.py

--- transmit.py
+++ transmit.py
@@ -5,7 +5,12 @@ VERSION = 145 # set version number
 
 laser = Pin(0, Pin.OUT) # init the laser pin
 
-clock = 0.2;
+clock = 0.02;
+
+laser.value(1)
+input()
+laser.value(0)
+time.sleep(10)
 
 def sync(x): # define sync function that takes in number of blinks
     for i in range(x):
@@ -16,15 +21,15 @@ def sync(x): # define sync function that takes in number of blinks
     laser.value(0)
         
         
-sync(5) # runs the sync function and makes the laser blink 4 times
+sync(10) # runs the sync function and makes the laser blink 4 times
 laser.value(0)
 
-time.sleep(3)
+time.sleep(4)
 
 packet = "" # creates the packet variable
 
-packet += '{0:08b}'.format(VERSION) # adds the version number to the packet
-packet = "1001010111010100"
+#packet += '{0:08b}'.format(VERSION) # adds the version number to the packet
+packet = "0110110001110101011000110110000101110011"
 laser.value(1)
 time.sleep(clock)
 for i in packet:
